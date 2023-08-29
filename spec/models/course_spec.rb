369a5d2af36f3074ea8ca06f5998c5762b9a566a
Sergio Souza <sergiosouzauff@gmail.com>
2023-08-29 19:08:33 -0300

    Included tests for relations

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -41,5 +41,8 @@ RSpec.describe Course do
   describe 'associations' do
     it { is_expected.to have_many(:teacher_assignments).dependent(:destroy) }
     it { is_expected.to have_many(:teachers).through(:teacher_assignments) }
+    it { is_expected.to have_many(:enrollments) }
+    it { is_expected.to have_many(:exams)}
+    it { is_expected.to have_many(:students).through(:enrollments) }
   end
 end
